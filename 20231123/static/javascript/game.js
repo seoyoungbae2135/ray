24b491ff8d9b9aacf74a31ae28b5a409fdb3f9ed
seoyoungbae2135 <ybsho62@naver.com>
2023-11-28 17:31:12 +0900

    vvv

--- 20231123/static/javascript/game.js
+++ 20231123/static/javascript/game.js
@@ -13,6 +13,10 @@ for(var i=1; i<=25; i++){
 
 $(function(){
 
+    $(".modal_bg").on("click",function(){
+        $("#result_modal").hide();
+    });
+
     $.each( board , function(i,v){  // i-인덱스, v-배열값(데이터값) i,v는 변수이다.
         $(".numBox").eq(i).text(v);
     });
@@ -32,31 +36,160 @@ $(function(){
 });
 
 function endgame(){
-    //var num1 = board.substring(0,5);
-    //for(var i=0; i<=board. )
-    if(board[0]==0){
-        var num1 = 1;
-    }
-    if(board[1]==0){
-        var num2 = 1;
-    }
-    if(board[2]==0){
-        var num3 = 1;
-    }
-    if(board[3]==0){
-        var num4 = 1;
-    }
-    if(board[4]==0){
-        var num5 = 1;
-    }
-    if(num1+num2+num3+num4+num5 == 5){
-        console.log("bingo");
-    }
+    // me
+    // let num1 = new Array();
+    // num1.push(board.substring(0,5));
     // console.log(num1);
-    // console.log(num2);
-    // console.log(num3);
-    // console.log(num4);
-    // console.log(num5);
 
-    //alert(board);
+    // if(board[0]==0 && board[1]==0 && board[2]==0 && board[3]==0 && board[4]==0){
+    //     console.log("bingo")
+    // }
+    // if(board[5]==0 && board[6]==0 && board[7]==0 && board[8]==0 && board[9]==0){
+    //     console.log("bingo")
+    // }
+    // if(board[10]==0 && board[11]==0 && board[12]==0 && board[13]==0 && board[14]==0){
+    //     console.log("bingo")
+    // }   
+    //for(var i=0; i<=board. )
+    // if(board[0]==0){
+    //     var num1 = 1;
+    // }
+    // if(board[1]==0){
+    //     var num2 = 1;
+    // }
+    // if(board[2]==0){
+    //     var num3 = 1;
+    // }
+    // if(board[3]==0){
+    //     var num4 = 1;
+    // }
+    // if(board[4]==0){
+    //     var num5 = 1;
+    // }
+    // if(num1+num2+num3+num4+num5 == 5){
+    //     console.log("bingo");
+        // num1=a, num2=0, num3=0, num4=0, num5=0;
+        //console.log(num1);
+    // }
+    // if(board[5]==0){
+    //     var num6 = 10;
+    // }
+    // if(board[6]==0){
+    //     var num7 = 10;
+    // }
+    // if(board[7]==0){
+    //     var num8 = 10;
+    // }
+    // if(board[8]==0){
+    //     var num9 = 10;
+    // }
+    // if(board[9]==0){
+    //     var num10 = 10;
+    // }
+    // if(num6+num7+num8+num9+num10 == 50){
+    //     console.log("bingo");
+    //  }
+    // if(board[10]==0){
+    //     var num11 = 100;
+    // }
+    // if(board[11]==0){
+    //     var num12 = 100;
+    // }
+    // if(board[12]==0){
+    //     var num13 = 100;
+    // }
+    // if(board[13]==0){
+    //     var num14 = 100;
+    // }
+    // if(board[14]==0){
+    //     var num15 = 100;
+    // }
+    // if(num11+num12+num13+num14+num15 == 500){
+    //     console.log("bingo");
+    // }
+    // if(board[15]==0){
+    //     var num16 = 1000;
+    // }
+    // if(board[16]==0){
+    //     var num17 = 1000;
+    // }
+    // if(board[17]==0){
+    //     var num18 = 1000;
+    // }
+    // if(board[18]==0){
+    //     var num19 = 1000;
+    // }
+    // if(board[19]==0){
+    //     var num20 = 1000;
+    // }
+    // if(num16+num17+num18+num19+num20 == 5000){
+    //     console.log("bingo");
+    // }
+    // if(board[20]==0){
+    //     var num21 = 1;
+    // }
+    // if(board[21]==0){
+    //     var num22 = 1;
+    // }
+    // if(board[22]==0){
+    //     var num23 = 1;
+    // }
+    // if(board[23]==0){
+    //     var num24 = 1;
+    // }
+    // if(board[24]==0){
+    //     var num25 = 1;
+    // }
+    // if(num21+num22+num23+num24+num25 == 5){
+    //     console.log("bingo");
+    // }
+    // if(num1+num6+num11+num16+num21 == 5){
+    //     console.log("bingo");
+    // }
+    // if(num2+num7+num12+num17+num22 == 5){
+    //     console.log("bingo");
+    // }
+    // if(num3+num8+num13+num18+num23 == 5){
+    //     console.log("bingo");
+    // }
+    // if(num4+num9+num14+num19+num24 == 5){
+    //     console.log("bingo");
+    // }
+    // if(num5+num10+num15+num20+num25 == 5){
+    //     console.log("bingo");
+    // }
+    // if(num1+num7+num13+num19+num25 == 5){
+    //     console.log("bingo");
+    // }
+    // if(num5+num9+num13+num17+num21 == 5){
+    //     console.log("bingo");
+    // }
+
+    // 2023.11.23 homework teacher
+        var row=0, col=0, end=0, cross=[0,0];
+        for(var i=0; i<5; i++ ){  //빙고 확인하기
+            for(var k=0; k<5; k++){
+                if(board[i*5+k]==0) row++; // 가로방향 5줄 전부확인
+                if(board[k*5+i]==0) col++; // 세로방향 5열 전부확인
+            }
+            if(board[i*6]==0) cross[0]++; // 왼->오 대각선
+            if(board[(i+1)*4]==0) cross[1]++; // 오->왼 대각선
+            if(cross[1]==5)end++;
+            if(cross[0]==5) end++;
+            if(row==5) end++;
+            if(col==5) end++;
+
+            row=0; // 한줄씩 확인 할 때 마다 0개부터 확인해야하므로 초기화
+            col=0; // 한열씩 확인 할 때 마다 0개부터 확인해야하므로 초기화
+        }
+
+        if (end==5){
+            $("#result_modal").show(); // $("#result_modal".css(display))
+            $(".result").text("5줄 빙고 완성");
+        }else if(end>=6){ //빙고실패
+            $("#result_modal").show();
+            $(".result").text("빙고실패");
+
+        }
+
 }
--- 20231127/static/javascript/sign.js
+++ 20231127/static/javascript/sign.js
@@ -2,15 +2,95 @@
 
 $(function(){
 
+
+
+    //select값 option 변경하기
+    $("#route option").eq(2).prop("selected",true); //eq(2):3번째 option
+
+    // 전부체크 체크박스에 체크시 전체 체크로 변하게 또는 반대상황
+    $("#all").on("click", function(){
+        let isCheck = $(this).is(":checked");
+        if(isCheck)
+            $(".ability_list").prop('checked',true);
+        else
+            $(".ability_list").prop('checked',false);
+    });
+
+    $(".ability_list").on("click",function(){
+        var chk_count = $(".ability_list:checked").length; //능력 체크박스에 체크된 갯수
+        var all_check = $(".ability_list").length; //능력 체크박스 총 갯수
+        if(chk_count == all_check) //체크된 갯수와 총갯수를 비교
+            $("#all").prop("checked",true); // 체크 갯수와 총 갯수가 같다면 전체 체크박스에 체크
+        else
+            $("#all").prop("checked",false); // 체크 갯수와 총 갯수가 다르면 전체 체크박스에 체크해제
+    });
+
+
+
+
     $("#mypw").on("keyup",function(){   //id mypw에 key 입력이되면 실행
         if($(this).val().length < 6 || $(this).val().length > 12){ 
             $(".pwvaild").text("비밀번호 6~12자 입니다.");
         }else{
-            $(".pwvaild").text('');
+            $(".pwvaild").text('비밀번호 안전');
         }
     })
 
+    $("#myphone").on("keyup",function(){
+        var num = $(this).val();
+
+        // 문자열음 기본구조가 배열의 구조이지만 자바스크립트에서 문자열 객체로 사용되기 때문에
+        // 객체전용 함수가 따로 존재한다.
+
+        // 숫자입력시 하이픈 생성코드
+        // if(num.length ==4 ){
+        //     num=num.slice(0,3)+"-"+num.slice(3);
+        //     $(this).val(num);
+        // }
+        // if(num.length == 9){
+        //     num=num.slice(0,8)+"-"+num.slice(8);
+        //     $(this).val(num);
+        // }
+
+        // 이것을 정규표현식으로 간단하게 하면
+
+        //num=num.replace(/[^0-9]/g, '').replace(/(\d{3})(\d{4})(\d{4})$/, `$1-$2-$3`); //`$1-$2-$3` 는 백틱
+        // 두번째 정규표현식
+        num=num.replace(/[^0-9]/g, '').
+            replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, `$1-$2-$3`).
+            replace(/(\-{1,2})$/g,""); //하이픈 삭제
+        $(this).val(num);
+
+        
+        
+    });
+
     $("#join").on("click",function(){   // id join을 클릭하면 실행
+
+        //파일업로드 확장자 검증
+        console.log( $("#myface")[0].files[0].name);
+        var fname = $("#myface")[0].files[0].name;
+        if( !fname.toLowerCase().includes(".png")){
+            alert("png형식의 이미지만 가능합니다.")
+        }
+
+        //select값 가져오기
+
+       var v = $("#route").val(); // $("#route option:selected").val();
+       alert(v);
+
+
+        // 체크박스에 체크된 값을 자바스크립트로 가져오는 코드
+        //var chk = $(".ability_list").eq(0).is(":checked");
+        //alert( chk);
+        var len = $(".ability_list:checked").length;
+        alert(len);
+
+        let chk = ''; // check값을 넣기위한 변수
+        $.each($(".ability_list:checked"), function(){ // 체크되어있는 checkbox만 반복문 처리
+            chk += $(this).val() +" ";
+        });
+        alert(chk);
 
             if($("#myid").val() == '' ){   //아이디를 입력하지않으면 
                 alert("아이디를 입력하세요");  // alert을 띄운다.
