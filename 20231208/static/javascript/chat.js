f32f871fab873f70dd23ac34fdff799242cebe24
seoyoungbae2135 <ybsho62@naver.com>
2023-12-11 09:03:27 +0900

    sss

--- 20231208/static/javascript/chat.js
+++ 20231208/static/javascript/chat.js
@@ -1,4 +1,18 @@
+let ref=0;
+$(function(){
+    $.getJSON("http://krdrive.ipdisk.co.kr:8000/test/data.php",function(data){
+        chat_show(data);
+    });
 
+    re = setTimeout(function(){
+        location.reload();
+    },5000);
+});
+
+$(document).on("keyup",function(){
+    if(re!=0)
+        clearTimeout(re);
+});
 
 function send(){
     var send_data = { writer:$("#writer").val() , content:$("#content").val()};
@@ -6,7 +20,7 @@ function send(){
 
     var xml = new XMLHttpRequest();
     xml.onload=function(){
-        var data = JSON.parse(this.response);
+        var data = JSON.parse(this.response); //JSON.parse : JSON 데이터 문자열을 객체(key와value)로 변환 
         chat_show(data);
     };
     xml.open("POST" , "http://krdrive.ipdisk.co.kr:8000/test/data.php");
@@ -16,9 +30,9 @@ function send(){
 
 function chat_show(data){
     var out="";
-    $each(data , function( i , msg){
-        out +=`<li class='hcat'><div class='write_info'>
-        <b class='name'>${msg.write}</b>
+    $.each(data , function( i , msg){
+        out +=`<li class='chat'><div class='write_info'>
+        <b class='name'>${msg.writer}</b>
         <span class='data'>${msg.data}</span></div>
         <div class='content'>${msg.content}</div><li>`;
     });
