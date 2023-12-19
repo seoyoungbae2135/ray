8aad76c3f73b933768fa9daefcd151ee5a2cec3f
seoyoungbae2135 <ybsho62@naver.com>
2023-12-19 16:47:05 +0900

    aaa

---
+++ study1/src/study1217/exercise1218.java
@@ -0,0 +1,9 @@
+package study1217;
+
+public class exercise1218 {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        System.out.printf("%d %d", a, b);
+        System.out.printf("%f %d")
+    }
+}
